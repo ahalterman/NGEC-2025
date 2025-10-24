e3cca7001019a1ad923825276f28c37c3674a1da
Andreas Beger <adbeger@gmail.com>
2025-10-24 10:30:55 +0300

    add check/warning for spacy models (related to #13)

--- NGEC/__init__.py
+++ NGEC/__init__.py
@@ -1,5 +1,46 @@
+
+import warnings
+
 from .event_class import EventClass
 from .actor_resolution import ActorResolver
 from .geolocation import GeolocationModel
 from .attribute_model import AttributeModel
 from .formatter import Formatter
+
+def _is_spacy_model_installed(model):
+    try:
+        from spacy import load
+        load(model)
+        return True
+    except (OSError):
+        return False
+
+
+def _check_spacy_models(models):
+    installed = [_is_spacy_model_installed(m) for m in models]
+
+    if all(installed):
+        return
+
+    # Construct warning message
+    msg = (
+        "\n" + "=" * 70 + "\n" 
+        + "WARNING: One or more required spaCy models are not installed:\n\n"
+        + "".join([f" - {m}\n" for m, inst in zip(models, installed) if not inst])
+        + "\nThese models are required for NGEC to fully function."
+        "\n" + "=" * 70 + "\n" 
+    )
+
+    warnings.warn(msg, UserWarning, stacklevel=2)
+
+
+def _is_huggingface_model_installed(model):
+    pass
+
+
+def _check_huggingface_models(models):
+    pass
+
+
+# Check on import
+_ = _check_spacy_models(["en_core_web_lg", "en_core_web_trf"])
